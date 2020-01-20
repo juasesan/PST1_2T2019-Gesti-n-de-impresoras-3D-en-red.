498f442feaaf2470985eb2cb05989fb23a7c9597
JoseParraga <jgparrag@espol.edu.ec>
2020-01-19 23:20:25 -0500

    Funcionamiento parcial para la parte del registro de los usuarios en la aplicacion

--- app/src/main/java/com/example/a3dprint_control/Ingresar.java
+++ app/src/main/java/com/example/a3dprint_control/Ingresar.java
@@ -1,16 +1,11 @@
 package com.example.a3dprint_control;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class Ingresar extends Activity {
 
